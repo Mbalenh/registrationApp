898fd726a38a267d87c60497bffe58745389dc20
Mbalenh <3408534@myuwc.ac.za>
2022-11-03 16:34:49 +0200

    added files

--- db/DbFunction.js
+++ db/DbFunction.js
@@ -3,25 +3,25 @@ const DbFunction = (db) =>{
 	const insertRegistration= async(reg) =>{
       let town = await db.oneOrNone('select * from Towns where town_name= $1',[reg.slice(0,2)])
 
-		await db.none('insert into registrationNumber (reg_number, town_id) values($1,$2);',[reg,town.id])
-}
- const getTownReg= async()=>{
-    	return await db.manyOrNone('SELECT * FROM Towns ')
+      await db.none('insert into registrationNumber (reg_number, town_id) values($1,$2);',[reg,town.id])
    }
-   const getRegNum= async()=>{
-    return await db.manyOrNone('SELECT DISTINCT reg_number FROM  registrationNumber')
-     
-    }  
-     
+   const getTownReg= async()=>{
+    return await db.manyOrNone('SELECT * FROM Towns ')
+ }
+ const getRegNum= async()=>{
+  return await db.manyOrNone('SELECT DISTINCT reg_number FROM  registrationNumber')
+  
+}  
 
-     const clearTownReg = async () => {
-        await db.none('Delete FROM registrationNumber')
-    }
 
-    const getRegFilter= async(value)=>{
-     return await db.manyOrNone('select DISTINCT reg_number from registrationNumber where town_id= $1',[value])
+const clearTownReg = async () => {
+ await db.none('Delete FROM registrationNumber')
+}
 
-    }
+const getRegFilter= async(value)=>{
+ return await db.manyOrNone('select DISTINCT reg_number from registrationNumber where town_id= $1',[value])
+
+}
 return{
    insertRegistration,
    getTownReg,
--- test/dbFunction.test.js
+++ test/dbFunction.test.js
@@ -2,17 +2,17 @@ const assert = require('assert');
 const pgp = require('pg-promise')();
 const DbFunction = require("../db/DbFunction")
 const DATABASE_URL= process.env.DATABASE_URL ||'postgresql://mbali:mba123@localhost:5432/registration'
-
+ console.log(DATABASE_URL)
 const config = { 
 
-connectionString : DATABASE_URL
+    connectionString : DATABASE_URL
 }
 
 
 if (process.env.NODE_ENV == 'production') {
-config.ssl = { 
-rejectUnauthorized : false
-}
+    config.ssl = { 
+        rejectUnauthorized : false
+    }
 
 }
 const db = pgp(config)
@@ -22,24 +22,24 @@ describe('The registration numbers app', function(){
     beforeEach(async function(){
  // clean the tables before each test run
 
-    await db.none('Delete FROM registrationNumber')
+ await db.none('Delete FROM registrationNumber')
  //setTimeout(10000)
 });
 
     it('should be able to add all registration numbers', async function(){
-         const dbFunction = DbFunction(db)
+     const dbFunction = DbFunction(db)
 
-        await dbFunction.insertRegistration('CA 123 456')
-        await dbFunction.insertRegistration('CJ 123-567')
+     await dbFunction.insertRegistration('CA 123 456')
+     await dbFunction.insertRegistration('CJ 123-567')
 
-        const regNumbers = await dbFunction.getRegNum()
+     const regNumbers = await dbFunction.getRegNum()
 
- assert.deepEqual([ { reg_number: 'CA 123 456' }, { reg_number: 'CJ 123-567' } ], regNumbers)
+     assert.deepEqual([ { reg_number: 'CA 123 456' }, { reg_number: 'CJ 123-567' } ], regNumbers)
 
-});
+ });
 
-it('should be able to add all registration numbers', async function(){
- 
+    it('should be able to add all registration numbers', async function(){
+
         const dbFunction = DbFunction(db)
 
         await dbFunction.insertRegistration('CY 123 656')
@@ -47,45 +47,45 @@ it('should be able to add all registration numbers', async function(){
         const regNumbers = await dbFunction.getRegNum()
         assert.deepEqual([ { reg_number: 'CY 123 656' }, { reg_number: 'CY 123567' } ], regNumbers)
 
-});
+    });
 
 
-it('should be able to check if the registration is from Bellville', async function(){
+    it('should be able to check if the registration is from Bellville', async function(){
 
 
- const dbFunction = DbFunction(db)
- await dbFunction.insertRegistration('CK 123 656')
+     const dbFunction = DbFunction(db)
+     await dbFunction.insertRegistration('CK 123 656')
 
-    await dbFunction.insertRegistration('CY 123567')
+     await dbFunction.insertRegistration('CY 123567')
 
-    const regNumbers = await dbFunction.getRegFilter(1)
-    assert.deepEqual([ {reg_number: 'CY 123567'} ], regNumbers)
+     const regNumbers = await dbFunction.getRegFilter(1)
+     assert.deepEqual([ {reg_number: 'CY 123567'} ], regNumbers)
 
-});
+ });
 
 
-it('should be able to check if the registration is from Paarl', async function(){
+    it('should be able to check if the registration is from Paarl', async function(){
 
- const dbFunction = DbFunction(db)
+     const dbFunction = DbFunction(db)
 
-await dbFunction.insertRegistration('CK 123 656')
-await dbFunction.insertRegistration('CK 123567')
+     await dbFunction.insertRegistration('CK 123 656')
+     await dbFunction.insertRegistration('CK 123567')
 
-const regNumbers = await dbFunction.getRegFilter(4)
+     const regNumbers = await dbFunction.getRegFilter(4)
 
-assert.deepEqual([ {reg_number: 'CK 123 656'}, {reg_number: 'CK 123567'} ], regNumbers)
-});
+     assert.deepEqual([ {reg_number: 'CK 123 656'}, {reg_number: 'CK 123567'} ], regNumbers)
+ });
 
-it('should be able to check if the registration is from Cape town', async function(){
-const dbFunction = DbFunction(db)
+    it('should be able to check if the registration is from Cape town', async function(){
+        const dbFunction = DbFunction(db)
 
-    await dbFunction.insertRegistration('CA 123 656')
-    await dbFunction.insertRegistration('CA 123567')
-    const regNumbers = await dbFunction.getRegFilter(2)
-    assert.deepEqual([ {reg_number: 'CA 123 656'}, {reg_number: 'CA 123567'} ], regNumbers)
-});
+        await dbFunction.insertRegistration('CA 123 656')
+        await dbFunction.insertRegistration('CA 123567')
+        const regNumbers = await dbFunction.getRegFilter(2)
+        assert.deepEqual([ {reg_number: 'CA 123 656'}, {reg_number: 'CA 123567'} ], regNumbers)
+    });
 
- after(function(){
- db.$pool.end()
-});
+    after(function(){
+     db.$pool.end()
+ });
 });
