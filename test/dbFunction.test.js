a8635701dbc8210c6e4378010a7c1df2a76a1a42
Mbalenh <3408534@myuwc.ac.za>
2022-11-04 11:25:03 +0200

    added files

--- test/dbFunction.test.js
+++ test/dbFunction.test.js
@@ -1,21 +1,20 @@
 const assert = require('assert');
 const pgp = require('pg-promise')();
 const DbFunction = require("../db/DbFunction")
-const DATABASE_URL= process.env.DATABASE_URL ||'postgresql://mbali:mba123@localhost:5432/registration'
- console.log(DATABASE_URL)
+
+const DATABASE_URL= process.env.DATABASE_URL ||"postgresql://mbali:mba123@localhost:5432/registration"
+
 const config = { 
-
-    connectionString : DATABASE_URL
+  connectionString : DATABASE_URL
 }
-
 
 if (process.env.NODE_ENV == 'production') {
-    config.ssl = { 
-        rejectUnauthorized : false
-    }
-
+  config.ssl = { 
+    rejectUnauthorized : false
+  }
 }
-const db = pgp(config)
+
+const db = pgp(config);
 
 
 describe('The registration numbers app', function(){
